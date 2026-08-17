abf52054d852f75de33bc4c2c60b2f80647d4d77
yvy976 <yvy976@vols.utk.edu>
2026-08-17 16:23:25 -0400

    Virtual Index Card

---
+++ script.bash
@@ -0,0 +1,9 @@
+# Name: Mung-Shu Shen
+# Fun Fact: Almonds are members of the peach family
+echo "Hello, My name is Mung-Shu. I am from Chattanooga, TN. I enjoy cooking."
+read -p
+echo "One thing that has kept me up at night, is that, if the human body is by nature hairy..."
+read -p
+echo "what is the need for soap; shampoo should be enough, right?"
+read -p
+echo "Thank you for coming to my TED talk."
